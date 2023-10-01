74af7297763d0458bd5ad35114a462658c7a716b
Warllen <warlenromero14@gmail.com>
2023-10-01 13:13:27 -0500

    last commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Profile from './components/Profile.js';
+import user from './user.json';
+import Statistics from './components/Statistics.js';
+import data from './data.json';
+import FriendList from './components/FriendList.js';
+import friends from './friends.json';
+import TransactionHistory from './components/TransactionHistory.js';
+import transactions from './transactions.json';
+
+function App() {
+  const { username, tag, location, avatar, stats } = user;
+
+  return (
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
+
+export default App;
+
+
